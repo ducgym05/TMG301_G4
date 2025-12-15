c3158576bd28031f6ef3f4f69f3b9c04b0569b62
ducgym05 <danggiaduc0905@gmail.com>
2025-12-15 19:29:11 +0700

    Delete DjangoChatbot directory

--- DjangoChatbot/chatbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.chatbot, name='chatbot'),
-    path('chat_response/', views.chat_response, name='chat_response'),
-]
